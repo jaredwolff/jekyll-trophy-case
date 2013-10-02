79d2d0a236af6d8f95e5ddd70d853e616913477a
Jared Wolff <jaredwolff@me.com>
2013-10-01 22:47:01 -0700

    [trophy case] adding check for thumbnail. raises error if a thumbnail does not exist.

--- trophy_case.rb
+++ trophy_case.rb
@@ -15,6 +15,9 @@ module Jekyll
       # First iterate through to find the featured posts
       for post in site.posts
         if count <= num_items
+
+          check_for_thumbnail(post)
+
           if post.data['featured'] and not featured_titles.include? post.title
             featured_titles << post.title
 
@@ -29,6 +32,9 @@ module Jekyll
 
       # Then iterate through to fill in the empty spaces (if they exist)
       for post in site.posts
+
+        check_for_thumbnail(post)
+
         if count <= num_items
           if not featured_titles.include? post.title
             featured_titles << post.title
@@ -41,6 +47,11 @@ module Jekyll
           return
         end
       end
+    end
+
+    # Checks to ensure a thumbnail is set for any post that is going into the trophy case
+    def check_for_thumbnail(post)
+      raise "Error: \"#{post.title}\" does not have a thumbnail image specified!" unless post.data['thumbnail']
     end
   end
 end
