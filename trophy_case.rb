11d7c3b35b07d45b958c4881ec81453c173eafec
Jared Wolff <jaredwolff@me.com>
2013-10-06 20:47:20 -0700

    Changing no thumbnail error to a warning. Allows posts with no thumbnails to be compiled. They will not be included in the Trophy Case.

--- trophy_case.rb
+++ trophy_case.rb
@@ -16,7 +16,9 @@ module Jekyll
       for post in site.posts
         if count <= num_items
 
-          check_for_thumbnail(post)
+          if !check_for_thumbnail(post)
+            next
+          end
 
           if post.data['featured'] and not featured_titles.include? post.title
             featured_titles << post.title
@@ -33,7 +35,9 @@ module Jekyll
       # Then iterate through to fill in the empty spaces (if they exist)
       for post in site.posts
 
-        check_for_thumbnail(post)
+        if !check_for_thumbnail(post)
+          next
+        end
 
         if count <= num_items
           if not featured_titles.include? post.title
@@ -51,7 +55,12 @@ module Jekyll
 
     # Checks to ensure a thumbnail is set for any post that is going into the trophy case
     def check_for_thumbnail(post)
-      raise "Error: \"#{post.title}\" does not have a thumbnail image specified!" unless post.data['thumbnail']
+      if post.data['thumbnail']
+        return true
+      else
+        puts "Warning: \"#{post.title}\" does not have a thumbnail image specified. It will not be included in the Trophy Case!"
+        return false
+      end
     end
   end
 end
